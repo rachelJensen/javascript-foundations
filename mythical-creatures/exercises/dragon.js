77e1aaa5e78d41f73b1115061795520e849241d8
Rachel Jensen <rachelangela@gmail.com>
2021-05-18 15:45:20 -0600

    Complete unicorn,js and dragon.js

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,24 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungryCount = 0;
+    this.hungry = true;
 
+
+  }
+
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+
+  eat() {
+    this.hungryCount += 1;
+    if (this.hungryCount >= 3) {
+      this.hungry = false;
+    }
+  }
+}
+
+
+module.exports = Dragon;
