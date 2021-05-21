03d0a1a4eff8cdec3a7b63ec150a215273238a11
Rachel Jensen <rachelangela@gmail.com>
2021-05-21 10:54:18 -0600

    Complete magician.js

---
+++ mythical-creatures/exercises/magician.js
@@ -0,0 +1,31 @@
+ class Magician {
+   constructor(magician) {
+     this.name = `The Great ${magician.name}`;
+     this.assistant = magician.assistant;
+     this.favoriteAccessory = magician.clothing || 'top hat';
+     this.confidencePercentage = 10;
+   }
+
+   performIncantation(incantation) {
+     return `${incantation.toUpperCase()}!`;
+   }
+
+   performTrick() {
+     this.confidencePercentage += 10;
+     if (this.favoriteAccessory !== 'top hat') {
+       return 'PULL DOVE FROM SLEEVE';
+     } else {
+     return 'PULL RABBIT FROM TOP HAT';
+     }
+   }
+
+   performShowStopper() {
+     if (this.confidencePercentage >= 100 && this.assistant) {
+       return 'WOW! The magician totally just sawed that person in half!';
+     } else {
+       return 'Oh no, this trick is not ready!';
+     }
+   }
+ }
+
+ module.exports = Magician;
