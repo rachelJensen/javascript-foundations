e3f4d397f27a6ecdb00abc15ca6527056fdc7500
Rachel Jensen <rachelangela@gmail.com>
2021-05-26 21:15:13 -0600

    Complete crafting

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,22 @@
 class Craft {
+  constructor(craftInfo) {
+    this.name = craftInfo.type;
+    this.materials = craftInfo.materials;
+    this.completed = false;
+  }
 
+  completeCraft() {
+    this.completed = true;
+    return 'All done! It looks great!';
+  }
+
+  calculateProjectTotal() {
+    var cost = 0;
+    for (var i = 0; i < this.materials.length; i++) {
+      cost += (this.materials[i].amount * this.materials[i].price);
+    }
+    return cost;
+  }
 }
 
 module.exports = Craft;
--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,24 @@
 class Material {
+  constructor(material, price, amount, units) {
+    this.name = material;
+    this.price = price;
+    this.amount = amount;
+    this.units = units;
+  }
 
+  useMaterial(amtUsed) {
+    if (amtUsed < this.amount) {
+      this.amount -= amtUsed;
+      return `You now have ${this.amount} ${this.units} of ${this.name} left.`
+    } else {
+      return `You don't have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`
+    }
+  }
+
+  calculateMaterialCost() {
+    var cost = this.price * this.amount;
+    return cost;
+  }
 }
 
 module.exports = Material;
