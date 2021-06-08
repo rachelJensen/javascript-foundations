a5c2431c309b4ae15015638c83a19dfa9a8ad666
Rachel Jensen <rachelangela@gmail.com>
2021-06-08 12:11:24 -0600

    Complete roadrace

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,5 +1,24 @@
 class Roadrace {
+  constructor(raceInfo) {
+    this.name = raceInfo.title;
+    this.location = raceInfo.city;
+    this.participants = [];
+  }
 
+  setDistance(distance) {
+    this.distance = distance;
+    return `The ${this.name} in ${this.location} is a ${distance} mile race.`;
+  }
+
+  registerParticipants(runner) {
+    this.participants.push(runner);
+  }
+
+  completeRace() {
+    for (var i = 0; i < this.participants.length; i++){
+      this.participants[i].runRace(this.name, this.distance);
+    }
+  }
 }
 
 module.exports = Roadrace;
